338ce76fce1ff9c74d9333d080903bbb9a2bc69c
Alexander Leitner <leitnersalex@gmail.com>
2018-07-04 11:48:50 -0400

    Added prev and next buttons; fix modal issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
     tag = params[:tag]
+    all_arts = Art.where(hidden: false).order(position: :asc)
     if (!tag.blank?)
-      @arts = Art.where(hidden: false).order(position: :asc).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 9)
+      @arts = all_arts.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 9)
     else
-      @arts = Art.where(hidden: false).order(position: :asc).paginate(:page => params[:page], :per_page => 9)
+      @arts = all_arts.paginate(:page => params[:page], :per_page => 9)
     end
   end
 end
